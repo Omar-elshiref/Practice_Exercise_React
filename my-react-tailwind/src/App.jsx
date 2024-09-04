3f6a281717d8353f32278da07e17b00f3a3ce1ed
Omar mohamed <omarabdou2002@yahoo.com>
2024-09-04 22:55:40 +0300

    Tailwind

---
+++ my-react-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'; // Import Tailwind CSS here
+
+
+function App() {
+  return (
+    <div className="bg-red-100 p-5 flex justify-center font-bold from-neutral-600 text-6xl m-12">
+      This is a React app with tailwind
+    </div>
+  );
+}
+
+
+export default App;
